834b59bed56ddea2a81e2c7da5983f482eb0a176
kozo yamagata <tune002@gmail.com>
2017-02-01 13:02:05 +0900

    Add bot_id to robot configuration list

--- lib/slappy/configuration/robot.rb
+++ lib/slappy/configuration/robot.rb
@@ -1,7 +1,7 @@
 module Slappy
   class Configuration
     class Robot
-      attr_accessor :botname, :username, :channel, :icon_emoji, :icon_url, :as_user
+      attr_accessor :botname, :username, :channel, :icon_emoji, :icon_url, :as_user, :bot_id
 
       def as_user
         @as_user || false
